a143b338c765532878e1dc38521697ba1d8db656
linegj <linekbv@gmail.com>
2025-01-27 09:58:34 +0100

    commit

--- js/script.js
+++ js/script.js
@@ -1,2 +1,27 @@
 "use strict";
 
+// variabler
+const min = 0;
+const max = 100;
+
+let targetNumber = Math.floor(Math.random() * (max - min) + min); // finder et tal mellem 0 to 100
+
+let attempts = 0; // antal forsøg
+let guess; // gæt
+let running = true; // boolean variabel
+
+while (running) {
+  guess = parseInt(prompt("Gæt et tal mellem 0 og 100:")); // prompt for at gætte et tal
+
+  if (guess === targetNumber) {
+    attempts++;
+    alert(`Tillykke, du gættede rigtigt! Det tog dig ${attempts} forsøg.`);
+    running = false;
+  } else if (guess < targetNumber) {
+    attempts++;
+    alert("Dit gæt er for lavt, prøv igen.");
+  } else if (guess > targetNumber) {
+    attempts++;
+    alert("Dit gæt er for højt, prøv igen.");
+  }
+}
